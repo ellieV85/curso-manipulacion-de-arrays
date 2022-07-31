1620ec8dffd30b87580c1cf5528d908543201b95
EllieV <elliev85@gmail.com>
2022-07-31 16:13:40 -0500

    Ejercicios para ejemplificar  Filter

---
+++ 03Filter.js
@@ -0,0 +1,48 @@
+const orders=[
+    {
+        customerName:"Nicolas",
+        total:60,
+        delivered:true,
+    },
+    {
+        customerName:"Zulema",
+        total:120,
+        delivered:false,
+    },
+    {
+        customerName:"Santiago",
+        total:180,
+        delivered:true,
+    },
+    {
+        customerName:"Valentina",
+        total:240,
+        delivered:false,
+    },
+];
+
+const taxes = orders.map(item => {
+    return {
+        ...item,
+        tax: item.total * 0.19,
+    }
+})
+
+const finalOrders = taxes.map(item => {
+    return {
+        ...item,
+        pagar: item.total + item.tax
+    }
+});
+
+const notDelivered = finalOrders.filter (item => item.delivered ? '' : 'false');
+
+notDelivered.forEach(order => {
+    bill.innerHTML += `<li class = 'bill--individual'> 
+    <p class='titulo'> Recibo de compra para ${order.customerName}, ojalá disfrutes tu árticulo. </p>
+    <input type= 'checkbox' ${order.delivered ? 'checked' : ''}/> <span> Tu orden fué entregada con éxito.</span>
+                            <p> El costo de tu compra es de: <span class='numeros'>$${order.total}</span> </p>
+                            <p class='impuestos'> Impuestos del 19% $${order.tax}</p>
+                            <p> Tu total a pagar es de: <span class='numeros'>$${order.pagar}</span> </p>
+                        </li>`
+});
